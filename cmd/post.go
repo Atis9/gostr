c36dda1c557455778775431b3a56e44e9e9c3b0d
Atis <atis@nyantech.com>
2023-11-01 00:12:29 +0900

    初期実装

--- cmd/post.go
+++ cmd/post.go
@@ -1,14 +1,21 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"os"
+	"regexp"
 
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
 	"github.com/spf13/cobra"
 )
+
+var nsec string
 
 // postCmd represents the post command
 var postCmd = &cobra.Command{
@@ -21,20 +28,94 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("post called")
+		m, err := cmd.Flags().GetString("message")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		_, v, err := nip19.Decode(nsec)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		privateKey := v.(string)
+
+		pub, _ := nostr.GetPublicKey(privateKey)
+		ev := nostr.Event{
+			PubKey:    pub,
+			CreatedAt: nostr.Now(),
+			Kind:      nostr.KindTextNote,
+			Tags:      nil,
+			Content:   m,
+		}
+
+		ev.Sign(privateKey)
+
+		ctx := context.Background()
+		for _, url := range []string{"wss://yabu.me", "wss://relay-jp.nostr.wirednet.jp"} {
+			relay, err := nostr.RelayConnect(ctx, url)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			_, err = relay.Publish(ctx, ev)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			fmt.Printf("published to %s\n", url)
+		}
 	},
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+	postCmd.PersistentFlags().StringP("message", "m", "", "post message")
+
 	rootCmd.AddCommand(postCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func initConfig() {
+	configHomePath := os.Getenv("XDG_CONFIG_HOME")
+	if configHomePath == "" {
+		configHomePath = os.Getenv("HOME") + "/.config"
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// postCmd.PersistentFlags().String("foo", "", "A help for foo")
+	gostrConfigDirPath := configHomePath + "/gostr"
+	if _, err := os.Stat(gostrConfigDirPath); os.IsNotExist(err) {
+		os.Mkdir(gostrConfigDirPath, 0755)
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// postCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	nsecFilePath := gostrConfigDirPath + "/nsec"
+	checkNsecFilePermission(nsecFilePath)
+	nsec = readNsec(nsecFilePath)
+}
+
+func checkNsecFilePermission(path string) {
+	f, err := os.Stat(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	nsecPerm := f.Mode().Perm()
+	if nsecPerm != 0600 {
+		fmt.Println("nsec file permission is not 600")
+		os.Exit(1)
+	}
+}
+
+func readNsec(path string) string {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+	re := regexp.MustCompile(`\r?\n`)
+
+	return re.ReplaceAllString(string(b), "")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -9,8 +8,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -37,15 +34,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gostr.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
